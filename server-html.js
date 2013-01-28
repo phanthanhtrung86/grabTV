fdcdd968bdfc17dbca9fe27a4d35d20174238437
Phan Thanh Trung <phanthanhtrung86@gmail.com>
2013-01-28 21:39:06 +0100

    fix the user count and time stamp on controller and client

--- server-html.js
+++ server-html.js
@@ -1,10 +1,6 @@
 var sys = require("sys");
 var express = require('express');
-//var ws = require('../lib/ws');
-/*var ws = require('../node_modules/ws');
-var server = ws.createServer({
-  debug: true
-});*/
+
 var app = require('express').createServer();
 // in this circumstance , we don't need io here
 //var io = require('socket.io').listen(app);
@@ -16,7 +12,6 @@ app.use(express.static(__dirname ));
 // in this circumstance , we don't need io here
 app.use(express.static('socket.io/lib' ));
 
-
 app.get('/video-controller.html', function (req, res) {
   res.sendfile((__dirname + '/video-controller.html'));
 });
@@ -27,42 +22,6 @@ app.get('/video-client.html', function (req, res) {
 
 var userCount = [];
 
-/*
-server.addListener("connection", function(conn){
-  server.broadcast("userCount " + ++userCount);
-  conn.addListener("message", function(message){
-    server.broadcast(message);
-  });
-});*/
-
-/*
-
-server.addListener("close", function(conn){
-  server.broadcast("userCount " + --userCount);
-});
-
-server.listen(8000, "localhost");
-
-};*/
-
-
-// in this circumstance , we don't need io here
-/*io.sockets.on('connection', function (socket) {
-
-        io.sockets.emit('message',"userCount " + ++userCount);
-		// when the client emits 'message', this listens and executes
-        socket.on('message', function (message) {
-                
-                io.sockets.emit('message',message);
-        });
-        // when the user disconnects.. perform this
-        socket.on('close', function(socket){
-                 io.sockets.emit('message',"userCount " + --userCount);
-        });
-
-               
-      
-});*/
 function log(msg) {
   sys.puts(+new Date + ' - ' + msg.toString());
 }
